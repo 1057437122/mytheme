d6fd3f0a8ef60e192019960cf5a50f39f8320845
leez <shiqiba7@163.com>
2014-02-16 13:51:21 +0800

    change the way to share from baidu to jiathis button

---
+++ includes/share.php
@@ -0,0 +1,23 @@
+<!-- JiaThis Button BEGIN -->
+<div class="jiathis_style_32x32">
+<a class="jiathis_button_qzone"></a>
+<a class="jiathis_button_tsina"></a>
+<a class="jiathis_button_tqq"></a>
+<a class="jiathis_button_renren"></a>
+<a class="jiathis_button_kaixin001"></a>
+<a href="http://www.jiathis.com/share" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank"></a>
+<a class="jiathis_counter_style"></a>
+</div>
+<script type="text/javascript" >
+var jiathis_config={
+	siteNum:12,
+	sm:"qzone,tsina,tqq,weixin,renren,kaixin001,douban,tieba,t163,cqq,fb,meilishuo",
+	summary:"",
+	boldNum:5,
+	shortUrl:false,
+	hideMore:false
+}
+</script>
+<script type="text/javascript" src="http://v3.jiathis.com/code/jia.js" charset="utf-8"></script>
+<!-- JiaThis Button END -->
+
--- s.php
+++ s.php
@@ -22,29 +22,8 @@
 						<?php the_content(); ?>
 			</div>
 			<div class="context_box">
-	<!-- Baidu Button BEGIN -->
-    <div id="bdshare" class="bdshare_t bds_tools_32 get-codes-bdshare">
-        <a class="bds_qzone"></a>
-        <a class="bds_tsina"></a>
-        <a class="bds_renren"></a>
-        <a class="bds_tqq"></a>
-        <a class="bds_baidu"></a>
-        <a class="bds_renren"></a>
-        <a class="bds_kaixin001"></a>
-        <a class="bds_hi"></a>
-        <a class="bds_tsohu"></a>
-        <a class="bds_qq"></a>
-        <a class="bds_tieba"></a>
-        <a class="bds_t163"></a>
-        <a class="bds_copy"></a>
-        <span class="bds_more">更多</span>
-    </div>
-<script type="text/javascript" id="bdshare_js" data="type=tools" ></script>
-<script type="text/javascript" id="bdshell_js"></script>
-<script type="text/javascript">
-	document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + new Date().getHours();
-</script>
-<!-- Baidu Button END --><div class="clr"></div>
+<?php include('includes/share.php');?>
+<div class="clr"></div>
 				<li>   该日志由 <?php the_author_posts_link(); ?> 于<?php the_time('Y年m月d日') ?>发表在 <?php the_category(', ') ?> 分类下，
 				<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {?>
 				你可以<a href="#respond">发表评论</a>，并在保留<a href="<?php the_permalink() ?>" rel="bookmark">原文地址</a>
