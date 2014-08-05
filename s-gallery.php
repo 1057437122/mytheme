5554f7ab22b66ff1a316245773b312ef6d8295da
whitelee <shiqiba7@163.com>
2014-08-05 22:45:34 +0800

    change the way to show the copyright about the contents

--- s-gallery.php
+++ s-gallery.php
@@ -44,16 +44,7 @@
 <?php include('includes/share.php');?>
 
 <div class="clr"></div>
-				<li>   该日志由 <?php the_author_posts_link(); ?> 于<?php the_time('Y年m月d日') ?>发表在 <?php the_category(', ') ?> 分类下，
-				<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {?>
-				你可以<a href="#respond">发表评论</a>，并在保留<a href="<?php the_permalink() ?>" rel="bookmark">原文地址</a>
-				及作者的情况下<a href="<?php trackback_url(); ?>" rel="trackback">引用</a>到你的网站或博客。
-				<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) { ?>
-				
-				<?php } ?></li>
-				<li>本文链接: <a href="<?php the_permalink() ?>" rel="bookmark" title="本文固定链接 <?php the_permalink() ?>"><?php the_title(); ?> | <?php bloginfo('name');?></a></li>
-				<li><?php the_tags('文章标签: ', ', ', ''); ?></li>
-				<li>版权所有: <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>-转载请标明出处</li>
+				<?php include('includes/copyright.php');?>
 </div><div class="clr"></div>
 		<?php include('includes/related.php'); ?>
 			<?php endwhile; ?>
