99f12391164b328ee7847195b92b33e9b9d05489
leez <shiqiba7@163.com>
2014-09-01 10:02:06 +0800

    change img server to sina

--- sidebar.php
+++ sidebar.php
@@ -16,29 +16,32 @@
 <?php include('includes/r_statistics.php'); ?>  
           
 </div> <div class="clr"></div> 
+<?php dynamic_sidebar('sidebar-1');?>
 <div class="widget" id="float">
-				<h3>与我相关</h3>
-				<ul class="contact">
-					
-					<li><a href="http://wee.leepine.com/aboutme">承接制作各种网站~</a></li>
-					<li><a href="http://wee.leepine.com/aboutme">二级域名服务~</a></li>
-					<li><a href="http://wee.leepine.com/aboutme">承接制作毕业画册~</a></li>
-					<li>欢迎关注微信，搜索Leez或者扫描以下：<br>
-					<img width="260px" height="260px"  src="http://img1.ph.126.net/oBuzg6dgTVufIhUIBCijOw==/6608919203468978410.jpg">
-					</li>
-					
-					
-				</ul> 
-         	</div>
-			
+	
+	</ul>
+	<h3>与我相关</h3>
+	<ul class="contact">
 
-			
-			
-			
+		<li><a href="http://wee.leepine.com/aboutme">承接制作各种网站~</a></li>
+		<li><a href="http://wee.leepine.com/aboutme">二级域名服务~</a></li>
+		<li><a href="http://wee.leepine.com/aboutme">承接制作毕业画册~</a></li>
+		<li>欢迎关注微信，搜索Leez或者扫描以下：<br>
+		<img width="260px" height="260px"  src="http://ww1.sinaimg.cn/mw690/9f2e29e5tw1ejwor08u9bj20zk0zkwi9.jpg">
+		</li>
 
-          <div class="clr"></div> 
+
+	</ul> 
+</div>
 
 
 
-		</div>
+
+
+
+<div class="clr"></div> 
+
+
+
+</div>
 
